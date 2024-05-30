9afc9bcd20a7f3e4f6750660ea8ac6c56ec46dad
sfh <sfhery@foxmail.com>
2024-05-30 09:55:22 +0800

    add extension

---
+++ SparkCom/Classes/Main/Extensions/Ex_Swift/Ex+/UIView+Extension.swift
@@ -0,0 +1,21 @@
+//
+//  UIView+Extension.swift
+//  SparkCom
+//
+//  Created by sfh on 2024/5/30.
+//
+
+import Foundation
+
+extension UIView {
+    /// 将当前视图转为UIImage
+    func asImage() -> UIImage {
+//        let format = UIGraphicsImageRendererFormat()
+//        format.scale = UIScreen.main.scale
+//        let renderer = UIGraphicsImageRenderer(bounds: bounds, format: format)
+        let renderer = UIGraphicsImageRenderer(bounds: bounds)
+        return renderer.image { rendererContext in
+            layer.render(in: rendererContext.cgContext)
+        }
+
+}
